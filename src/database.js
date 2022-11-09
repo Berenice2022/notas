48fd2f5428fd32812f40e03304ac28ac3179d581
Berenice Ortiz <bortiz.isc@gmail.com>
2022-11-09 11:24:33 -0600

    Conexion MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,16 @@
-const mongose = require ('mongoose');//mongodb
+const mongoose = require ('mongoose');//mongodb
+require('dotenv').config();
 
-mongose.connect('mongodb://localhost/notasdb')
+const url = process.env.MONGODB_URL
+
+mongoose.connect(url)
+        .then( ()=>{
+    console.log("Conectada a la base de datos")
+        }).catch( (err)=>{
+    console.log(err);
+})
+
+/*mongoose.connect('mongodb://localhost/notasdb')
 .then(db=> console.log("Base de datos conectada"))
-.catch(err => console.log(err));
+.catch(err => console.log(err));*/
  
